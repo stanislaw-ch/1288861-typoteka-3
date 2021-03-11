58c4c25c0ea5f840afd222566684d018348c536c
Stanislaw Chirko <post.chirko@gmail.com>
2021-03-11 13:20:00 +0100

    выполняет задание 6. Личный проект: генератор публикаций

---
+++ src/service/cli/help.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  name: `--help`,
+  run() {
+    const text = `
+    Программа запускает http-сервер и формирует файл с данными для API.
+    Гайд:
+      server <command>
+      Команды:
+      --version:            выводит номер версии
+      --help:               печатает этот текст
+      --generate <count>    формирует файл mocks.json
+    `;
+    console.log(text);
+  }
+};
---
+++ src/utils.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports.getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+module.exports.shuffle = (someArray) => {
+  for (let i = someArray.length - 1; i > 0; i--) {
+    const randomPosition = Math.floor(Math.random() * i);
+    [someArray[i], someArray[randomPosition]] = [someArray[randomPosition], someArray[i]];
+  }
+
+  return someArray;
+};
+
+module.exports.getRandomDate = () => {
+  const startPoint = new Date().getTime();
+
+  const endPoint = startPoint - new Date(90 * (24 * 3600 * 1000)).getTime();
+
+  const options = {
+    day: `numeric`,
+    month: `numeric`,
+    year: `numeric`,
+    hour: `numeric`,
+    minute: `numeric`,
+    second: `numeric`
+  };
+
+  const date = new Date(endPoint + Math.random() * (startPoint - endPoint)).toLocaleDateString(`ru-RU`, options);
+
+  return date;
+};
