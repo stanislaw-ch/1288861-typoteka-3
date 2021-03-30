12b2d5b1732cf2335aa5fc6b397e4ffa3d53534b
Stanislaw Chirko <post.chirko@gmail.com>
2021-03-30 20:44:48 +0200

    выполняет задание 18. Личный проект: другой сервер

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const chalk = require(`chalk`);
-const http = require(`http`);
+const express = require(`express`);
 const fs = require(`fs`).promises;
+const {HttpCode} = require(`../../constants`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const {HttpCode} = require(`../../constants`);
+
+const app = express();
+app.use(express.json());
+
+app.get(`/offers`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+  }
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
@@ -15,53 +32,12 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    const sendResponse = (res, statusCode, message) => {
-      const template = `
-        <!Doctype html>
-          <html lang="ru">
-          <head>
-            <title>With love from Node</title>
-          </head>
-          <body>${message}</body>
-        </html>`.trim();
-
-      res.statusCode = statusCode;
-      res.writeHead(statusCode, {
-        'Content-Type': `text/html; charset=UTF-8`,
-      });
-
-      res.end(template);
-    };
-
-    const onClientConnect = async (req, res) => {
-      const notFoundMessageText = `Not found`;
-
-      switch (req.url) {
-        case `/`:
-          try {
-            const fileContent = await fs.readFile(FILENAME);
-            const mocks = JSON.parse(fileContent);
-            const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-            sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-          } catch (err) {
-            sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          }
-
-          break;
-        default:
-          sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          break;
-      }
-    };
-
-    http.createServer(onClientConnect)
-        .listen(port)
-        .on(`error`, (err) => {
-          if (err) {
-            return console.error(`Ошибка при создании сервера`, err);
-          }
-
-          return console.info(chalk.green(`Ожидаю соединений на ${port}`));
-        });
+    app.listen(port)
+    .on(`listening`, () => {
+      return console.info(chalk.green(`Ожидаю соединений на ${port}`));
+    })
+    .on(`error`, (err) => {
+      return console.error(`Ошибка при создании сервера`, err);
+    });
   }
 };
